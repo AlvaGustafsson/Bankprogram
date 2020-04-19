e9bbf339a69c447186bf94363e59eb1a1cab9988
Alva Gustafsson <algu01002@utb.vaxjo.se>
2020-04-19 23:00:54 +0200

    Commit 1

---
+++ Account.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Account
+{
+    private double balance;
+    private Customer owner;
+    private long accountNumber;
+
+	public Account(Customer c, int n)
+	{
+        owner = c;
+        accountNumber = parseLong("8000" + (100000 + n));
+	}
+
+    public bool deposit(double amount)
+    {
+        if(amount > 0)
+            balance += amount;
+        else
+            return false;
+
+        return true;
+    }
+}
